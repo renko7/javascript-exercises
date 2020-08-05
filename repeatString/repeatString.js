ffb50274f6c2afd53b3a17e9b609153daf02d1e7
renko7 <jotakaemail@gmail.com>
2020-08-05 00:18:12 -0400

    Practiced with Jasmine
    If you get a single green dot at the end of Jasmine then that means you passed all the test-cases.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, amount) {
+    let newStr = "";
+    while (amount !==0){
+        newStr+=str;
+        amount--;
+    }
+    return newStr;
 }
 
 module.exports = repeatString
